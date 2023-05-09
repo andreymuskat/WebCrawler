d956fb66f0c306bc6c7bed780025243a6d54a6d1
andreymuskat <andrey.strel.2001@gmail.com>
2023-05-09 14:57:54 +0300

    first part

---
+++ WebCrawler/WebCrawler/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace WebCrawler
+{
+    public class Game
+    {
+        public string NameGame { get; set; }
+
+        public string NameAuthor { get; set; }
+
+        public string Rating { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Link { get; set; }
+    }
+}
---
+++ WebCrawler/WebCrawler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main()
+        {
+            startCrawlerAsync();
+            Console.ReadLine();
+        }
+
+        private static async Task startCrawlerAsync()
+        {
+            var url = "https://tesera.ru/location/russia/moscow/wants/";
+            var httpCLient = new HttpClient();
+            var html = await httpCLient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var games = new List<Game>();
+
+
+            var divs = htmlDocument.DocumentNode.Descendants("div")
+                .Where(a => a.GetAttributeValue("class", "").Equals("gameslinked")).ToList();
+
+            try
+            {
+                foreach (var div in divs)
+                {
+                    var game = new Game()
+                    {
+                        NameGame = div.SelectSingleNode("//h3/a").InnerText,
+                        NameAuthor = div.Descendants("a").First(x => x.Attributes["class"] != null && x.Attributes["class"].Value == "spic").InnerText,
+                        Rating = div.Descendants("span").FirstOrDefault().InnerText,
+                        ImageUrl = div.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value,
+                        Link = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value,
+                    };
+                    games.Add(game);
+                }
+            }
+            catch
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
