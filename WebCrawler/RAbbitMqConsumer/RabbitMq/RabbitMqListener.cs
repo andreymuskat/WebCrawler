aa3fb1ba3007cfa92556cffe4b0de1aae08d030d
andreymuskat <andrey.strel.2001@gmail.com>
2023-05-22 23:24:49 +0300

    Add Consumer in project

---
+++ WebCrawler/RAbbitMqConsumer/RabbitMq/RabbitMqListener.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+using WebCrawler;
+
+public class RabbitMqListener : BackgroundService
+{
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public RabbitMqListener()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _channel.QueueDeclare(queue: "game_tasks", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            var game = JsonConvert.DeserializeObject<GameNamesEntity>(message);
+
+            // Обработка полученной задачи
+            ProcessGame(game);
+
+            _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        };
+
+        _channel.BasicConsume(queue: "game_tasks", autoAck: false, consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    private void ProcessGame(GameNamesEntity game)
+    {
+
+        Console.WriteLine($"Received Game: {game.NameGame}, Author: {game.NameAuthor}");
+    }
+
+    public override void Dispose()
+    {
+        _channel.Close();
+        _connection.Close();
+        base.Dispose();
+    }
+}
